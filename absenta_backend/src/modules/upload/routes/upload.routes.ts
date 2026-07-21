72a8fc7763c9aedc3dce4bd7a26da191bbe95874
sharemovie1993 <sharemovie1993@gmail.com>
2026-07-21 11:44:40 +0700

    fix(backend): use valid RoleName enum values and correct import paths in guru and upload routes

--- absenta_backend/src/modules/upload/routes/upload.routes.ts
+++ absenta_backend/src/modules/upload/routes/upload.routes.ts
@@ -1,10 +1,10 @@
 import { uploadController } from '../controllers/upload.controller';
-import { requireCapability } from '../../../../middlewares/requireCapability';
-import { RoleName } from '../../../../constants/enums';
+import { requireCapability } from '../../../middlewares/requireCapability';
+import { RoleName } from '../../../constants/enums';
 
 export async function uploadRoutes(fastify: any) {
   fastify.post('/', {
-    preHandler: [requireCapability('documents.upload', { exemptRoles: [RoleName.GURU, RoleName.ADMIN_SEKOLAH, RoleName.STAF, RoleName.SISWA] })],
+    preHandler: [requireCapability('documents.upload', { exemptRoles: [RoleName.GURU, RoleName.ADMIN, RoleName.SUPERADMIN, RoleName.SISWA] })],
     handler: uploadController.uploadFile
   });
 }
